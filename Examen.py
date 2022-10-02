d223035734bdbd4428abe61fee517d11a4c210ff
Alfred-L96 <60676849+Alfred-L96@users.noreply.github.com>
2022-10-02 18:21:14 -0500

    Update Examen.py

--- Examen.py
+++ Examen.py
@@ -5,35 +5,38 @@ from tkinter.tix import Tree
 import cv2
 import os.path as path
 import numpy as np
-#Alumno: Abdiel Jefte Santillan Luna
-CapPantalla = cv2.VideoCapture(0)
-n='0'
-nn =1
-if not CapPantalla.isOpened():
+
+CPantalla = cv2.VideoCapture(0)
+n = '0'
+nn = 1
+if not CPantalla.isOpened():
     raise IOError("No se puede abrir la camara")
 while True:
-    ret, frame = CapPantalla.read()
+    ret, frame = CPantalla.read()
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input',frame)
+    cv2.imshow('Input', frame)
     c = cv2.waitKey(1)
-    
+
     if c == 27:
         break
     elif c == 32:
-        CapPantalla.set(cv2.CAP_PROP_FRAME_WIDTH,720) # ancho
-        CapPantalla.set(cv2.CAP_PROP_FRAME_HEIGHT,640) # alto
+        CPantalla.set(cv2.CAP_PROP_FRAME_WIDTH, 720)  # ancho
+        CPantalla.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)  # alto
         NombreImagen = "rostro"
         ExtencionDeImagen = ".jpg"
-        image = 'C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/rostro'+n+ExtencionDeImagen
+        image = 'C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/rostro' + n + ExtencionDeImagen
         if path.exists(image):
             while path.exists(image):
                 nn = nn + 1
-                n=str(nn)
+                n = str(nn)
                 print(n)
-                image = 'C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/rostro'+n+ExtencionDeImagen
+                image = 'C:/Users/Chichona/Pictures/Rostro' + n + ExtencionDeImagen
                 if path.exists(image) != True:
-                    cv2.imwrite('C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/'+NombreImagen+n+ExtencionDeImagen,frame)
-                    img = cv2.imread('C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/'+NombreImagen+n+ExtencionDeImagen)
+                    cv2.imwrite(
+                        'C:/Users/Chichona/Pictures/' + NombreImagen + n + ExtencionDeImagen,
+                        frame)
+                    img = cv2.imread(
+                        'C:/Users/Chichona/Pictures/' + NombreImagen + n + ExtencionDeImagen)
                     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                     gray = cv2.medianBlur(gray, 5)
                     edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
@@ -41,13 +44,18 @@ while True:
 
                     # 3) Cartoon
                     cartoon = cv2.bitwise_and(color, color, mask=edges)
-                    
-                    cv2.imwrite('C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/rostroCartun'+n+ExtencionDeImagen,cartoon)
+
+                    cv2.imwrite(
+                        'C:/Users/Chichona/Pictures/rostroCartoon' + n + ExtencionDeImagen,
+                        cartoon)
                     cv2.imshow("Cartoon", cartoon)
                     break
         else:
-            cv2.imwrite('C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/'+NombreImagen+n+ExtencionDeImagen,frame)
-            img = cv2.imread('C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/'+NombreImagen+n+ExtencionDeImagen)
+            cv2.imwrite(
+                'C:/Users/Chichona/Pictures/' + NombreImagen + n + ExtencionDeImagen,
+                frame)
+            img = cv2.imread(
+                'C:/Users/Chichona/Pictures/' + NombreImagen + n + ExtencionDeImagen)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             gray = cv2.medianBlur(gray, 5)
             edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
@@ -55,10 +63,11 @@ while True:
 
             # 3) Cartoon
             cartoon = cv2.bitwise_and(color, color, mask=edges)
-            
-            cv2.imwrite('C:/Users/santi/Procesamiento de imagenes/Examen1raParcial/rostroCartun'+n+ExtencionDeImagen,cartoon)
-            cv2.imshow("Cartoon", cartoon)
-      
 
-CapPantalla.release()
+            cv2.imwrite(
+                'C:/Users/Chichona/Pictures/rostroCartoon' + n + ExtencionDeImagen,
+                cartoon)
+            cv2.imshow("Cartoon", cartoon)
+
+CPantalla.release()
 cv2.destroyAllWindows()
